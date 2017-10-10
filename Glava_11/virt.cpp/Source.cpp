69f61e0acb4398fa849012ea190366a67e008f88
9weiss6 <9weiss6@gmail.com>
2017-10-10 15:49:36 +0500

    Added 11 Chapter

---
+++ Glava_11/virt.cpp/Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+	virtual void show()
+	{
+		cout << "base\n";
+
+	}
+
+	/*
+
+	virtual void show() = 0;
+
+	*/
+};
+
+class Dev1 : public Base
+{
+public:
+	void show()
+	{
+		cout << "dev1\n";
+	}
+};
+
+class Dev2 : public Base
+{
+public:
+	void show()
+	{
+		cout << "dev2\n";
+	}
+};
+
+int main()
+{
+	Dev1 dv1;
+	Dev2 dv2;
+	Base* ptr;
+
+	
+	ptr = &dv1;
+	ptr->show();
+	ptr = &dv2;
+	ptr->show();
+
+	system("pause");
+	return 0;
+}
